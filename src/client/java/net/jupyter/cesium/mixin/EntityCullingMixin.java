a34fbd34e888026294d0fcb9b26738e545359383
Jupyter0 <98959954+Jupyter0@users.noreply.github.com>
2026-03-18 15:59:56 -0400

    Split Client/Server Mixins

---
+++ src/client/java/net/jupyter/cesium/mixin/EntityCullingMixin.java
@@ -0,0 +1,9 @@
+package net.jupyter.cesium.mixin;
+
+import net.minecraft.client.render.WorldRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(WorldRenderer.class)
+public class EntityCullingMixin {
+
+}
---
+++ src/client/java/net/jupyter/cesium/mixin/GreedyMeshingMixin.java
@@ -0,0 +1,9 @@
+package net.jupyter.cesium.mixin;
+
+import net.minecraft.client.render.chunk.ChunkBuilder;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ChunkBuilder.class)
+public class GreedyMeshingMixin {
+
+}
