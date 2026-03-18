45701f8c292376eaa6f7f9e0c3d652be62b6d27c
Jupyter0 <98959954+Jupyter0@users.noreply.github.com>
2026-03-18 14:51:02 -0400

    Initial commit

---
+++ src/main/java/net/jupyter/cesium/CesiumClient.java
@@ -0,0 +1,10 @@
+package net.jupyter.cesium;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CesiumClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/jupyter/cesium/CesiumDataGenerator.java
@@ -0,0 +1,11 @@
+package net.jupyter.cesium;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CesiumDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
